e5860170df571e9f0cc8cf926ae557b21c4d9af1
Feanor306 <feanor306@gmail.com>
2020-02-17 12:08:57 +0100

    Burger Builder Init Commit

---
+++ src/components/Order/Order.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import classes from './Order.css'
+
+const order = (props) => {
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ ingredientName ]
+        });
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return <span 
+                key={ ig.name }
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 .5rem',
+                    border: '1px solid #ccc',
+                    padding: '.3rem'
+                }}>
+                { ig.name } ({ ig.amount })
+                </span>;
+    });
+
+    return (
+        <div className={ classes.Order }>
+            <p>Ingredients: { ingredientOutput }</p>
+            <p>Price: <strong>USD { Number.parseFloat(props.price).toFixed(2) }</strong></p>
+        </div>
+    );
+};
+
+export default order;
